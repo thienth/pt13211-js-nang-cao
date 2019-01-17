a9efc482e3b2c57be33270ac59e4ea8db9a2cdf3
thienth32 <thienth32@gmail.com>
2019-01-17 13:32:28 +0700

    update

---
+++ lesson1/main.js
@@ -0,0 +1,14 @@
+var name = 'thienth';
+let age = 29;
+var isMarried = false;
+var seasonClasses = ['PT13211', 'PT13313'];
+var testNull = null;
+var people = {
+    username: "your username",
+    password: "123456",
+    login: function(){
+        console.log("chuc mung ban da dang nhap thanh cong");
+    }
+};
+
+console.log(name, age, isMarried, seasonClasses, testNull, people);
