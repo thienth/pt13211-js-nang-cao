ec7bd48cb4d0379abc972fa2f09ea5f55b8c10c7
thienth32 <thienth32@gmail.com>
2019-02-16 12:19:37 +0700

    update lesson 6

---
+++ shop/public/js/question.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+        q: "Sông nào dài nhất Việt Nam?",
+        a: "songcuulong"
+    },
+    {
+        q: "Núi cao nhất ở Việt Nam tên là gì?",
+        a: "fanxipang"
+    },
+    {
+        q: "Tỉnh nào rộng nhất Việt Nam?",
+        a: "hanoi"
+    }
+];
+var bangChuCai = [
+    "a", "b", 'c', 'd', 'o'
+]
+
